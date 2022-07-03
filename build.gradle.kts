db4ec9fba671a54dd5284f649c5a282ee61ecc49
xHyroM <generalkubo@gmail.com>
2022-07-03 18:56:50 +0200

    fix build.gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.papermc.paperweight.util.constants.*
+
 plugins {
     java
     `maven-publish`
@@ -5,43 +7,11 @@ plugins {
     id("io.papermc.paperweight.patcher") version "1.3.8"
 }
 
-val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
-
-allprojects {
-    apply(plugin = "java")
-    apply(plugin = "maven-publish")
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
-        }
-    }
-}
-
-subprojects {
-    tasks.withType<JavaCompile>().configureEach {
-        options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
-    }
-    tasks.withType<Javadoc> {
-        options.encoding = Charsets.UTF_8.name()
-    }
-    tasks.withType<ProcessResources> {
-        filteringCharset = Charsets.UTF_8.name()
-    }
-
-    repositories {
-        mavenCentral()
-        maven(paperMavenPublicUrl)
-        maven("https://jitpack.io")
-    }
-}
-
 repositories {
     mavenCentral()
-    maven(paperMavenPublicUrl) {
+    maven("https://papermc.io/repo/repository/maven-public/") {
         content {
-            onlyForConfigurations(configurations.paperclip.name)
+            onlyForConfigurations(PAPERCLIP_CONFIG)
         }
     }
 }
@@ -52,51 +22,62 @@ dependencies {
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+}
+
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+
+}
+
 paperweight {
     serverProject.set(project(":sharkur-server"))
 
-    remapRepo.set(paperMavenPublicUrl)
-    decompileRepo.set(paperMavenPublicUrl)
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
 
-    usePaperUpstream(providers.gradleProperty("purpurCommit")) {
-        withPaperPatcher {
+    useStandardUpstream("purpur") {
+        url.set(github("PurpurMC", "Purpur"))
+        ref.set(providers.gradleProperty("purpurCommit"))
+
+        withStandardPatcher {
+            baseName("Purpur")
+
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("Sharkur-API"))
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Sharkur-Server"))
-        }
-    }
-}
-
-tasks.generateDevelopmentBundle {
-    apiCoordinates.set("org.sharkurmc.sharkur:sharkur-api")
-    mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
-    libraryRepositories.set(
-            listOf(
-                    "https://repo.maven.apache.org/maven2/",
-                    paperMavenPublicUrl,
-                    "https://repo.purpurmc.org/snapshots",
-                    "https://reposilite.cezarsalat.tk/snapshots"
-            )
-    )
-}
-
-allprojects {
-    publishing {
-        repositories {
-            maven("https://reposilite.cezarsalat.tk/snapshots") {
-                name = "sharkur"
-                credentials(PasswordCredentials::class)
-            }
-        }
-    }
-}
-
-publishing {
-    publications.create<MavenPublication>("devBundle") {
-        artifact(tasks.generateDevelopmentBundle) {
-            artifactId = "dev-bundle"
         }
     }
 }
