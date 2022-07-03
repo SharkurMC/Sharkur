943917993e40ab9a076c07b03c808819fc11a8b4
xHyroM <generalkubo@gmail.com>
2022-07-03 18:47:46 +0200

    new build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import io.papermc.paperweight.util.constants.*
-
 plugins {
     java
     `maven-publish`
@@ -7,23 +5,11 @@ plugins {
     id("io.papermc.paperweight.patcher") version "1.3.8"
 }
 
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-            onlyForConfigurations(PAPERCLIP_CONFIG)
-        }
-    }
-}
+val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
 
-dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
-    paperclip("io.papermc:paperclip:3.0.2")
-}
-
-subprojects {
+allprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     java {
         toolchain {
@@ -34,50 +20,83 @@ subprojects {
 
 subprojects {
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
-
     tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
     }
-
     tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
 
     repositories {
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven(paperMavenPublicUrl)
         maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
+}
 
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content {
+            onlyForConfigurations(configurations.paperclip.name)
+        }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
+    paperclip("io.papermc:paperclip:3.0.2")
 }
 
 paperweight {
     serverProject.set(project(":sharkur-server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    remapRepo.set(paperMavenPublicUrl)
+    decompileRepo.set(paperMavenPublicUrl)
 
-    useStandardUpstream("purpur") {
-        url.set(github("PurpurMC", "Purpur"))
-        ref.set(providers.gradleProperty("purpurCommit"))
-
-        withStandardPatcher {
-            baseName("Purpur")
-
+    usePaperUpstream(providers.gradleProperty("purpurCommit")) {
+        withPaperPatcher {
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("Sharkur-API"))
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Sharkur-Server"))
+        }
+    }
+}
+
+tasks.generateDevelopmentBundle {
+    apiCoordinates.set("org.sharkurmc.sharkur:sharkur-api")
+    mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
+    libraryRepositories.set(
+            listOf(
+                    "https://repo.maven.apache.org/maven2/",
+                    paperMavenPublicUrl,
+                    "https://repo.purpurmc.org/snapshots",
+                    "https://reposilite.cezarsalat.tk/snapshots"
+            )
+    )
+}
+
+allprojects {
+    publishing {
+        repositories {
+            maven("https://reposilite.cezarsalat.tk/snapshots") {
+                name = "sharkur"
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("devBundle") {
+        artifact(tasks.generateDevelopmentBundle) {
+            artifactId = "dev-bundle"
         }
     }
 }
